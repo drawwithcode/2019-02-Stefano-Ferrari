affaed54992a868a16e1b66fd518cb5e69fa5d9a
Stefano-Ferrari <36973253+Stefano-Ferrari@users.noreply.github.com>
2019-09-30 16:47:57 +0200

    Texture
    I edited the html file to remove the scrollbar and wrote a js script to make a random psychedelic texture downloadable with the press of the spacebar.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,58 @@
-function preload(){
-  // put preload code here
+var n = 0;
+var R;
+var G;
+var B;
+
+function preload() {
+
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  noStroke();
+
 }
 
 function draw() {
-  // put drawing code here
+
+  var lato = Math.floor(windowWidth / 100);
+
+  background(10);
+
+
+  for (var i = lato / 2; i < windowWidth; i += lato) {
+    for (var j = lato / 2; j < windowHeight; j += lato) {
+
+      if (mouseIsPressed) {
+        var l = lato * noise(100 / (sqrt(Math.pow(i - mouseX, 2) + Math.pow(j - mouseY, 2))));
+      } else {
+        R = random(255);
+        G = random(255);
+        B = random(255);
+        fill(R, G, B);
+        var l = lato * noise(random(-50, 50));
+      }
+
+      rect(i, j, l, l);
+
+    }
+  }
+  if (keyIsDown(32)) {
+    screenshot();
+  }else{
+  fill(10);
+  rect((windowWidth/2)-1,(windowHeight/20)-2,280,18);
+  fill(255);
+  textSize(10);
+  textAlign(CENTER);
+  text('Click to focus the noise. Press Spacebar to save the texture.', windowWidth/2, windowHeight/20);
+  fill(R, G, B);
+}
+}
+
+function screenshot() {
+if (keyIsDown(32)) {
+  save('texture.jpg');
+}
 }
